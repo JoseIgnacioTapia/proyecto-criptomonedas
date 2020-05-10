46d14b25589e3b94066e7c46394f4050bf069509
José Ignacio Tapia <30639989+crashToxic@users.noreply.github.com>
2020-05-10 20:46:14 -0300

    Obteniendo los API Keys, validando el formulario y mostrando un mensaje de error

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const ui = new Interfaz();
+
+
+// leer el formulario
+const formulario = document.querySelector('#formulario');
+
+// eventlistener
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // leer la moneda seleccionada
+    const monedaSelect = document.querySelector('#moneda');
+    const monedaSelecionada = monedaSelect.options[monedaSelect.selectedIndex].value;
+
+    // leer la criptomoneda seleccionada
+    const criptoMonedaSelect = document.querySelector('#criptomoneda');
+    const criptoMonedaSeleccionada = criptoMonedaSelect.options[criptoMonedaSelect.selectedIndex].value;
+
+    // comprobar que ambos campos tengan algo seleccionado
+    if(monedaSelecionada === '' || criptoMonedaSeleccionada === '') {
+        // arrojar una alerta de error
+        ui.mostrarMensaje('Ambos Campos son Obligatorios', 'alert bg-danger text-center');
+    } else {
+        // todo bien, consultar la api
+    }
+
+});
